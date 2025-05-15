374b2e138a007365106afcf78af3b687494e204d
Skewber <jeremia.e.neuhold@gmail.com>
2025-05-15 17:41:42 +0200

    slight rework of image stacking

--- darclight/reducer.py
+++ darclight/reducer.py
@@ -1,11 +1,11 @@
 import numpy as np
 from astropy.stats import sigma_clip
 from .io import Observation
-from shutil import copy
 
 class Reducer():
     def __init__(self, data:Observation):
         self.data = data
+        self.method = 'sigmaclip'
 
     @staticmethod
     def combine(data:list[np.ndarray], method='sigmaclip', sigma:int=5):
@@ -24,18 +24,9 @@ class Reducer():
             case 'add':
                 return np.sum(stack, axis=0)
 
-    @staticmethod
-    def stack_images(to_combine:list[np.ndarray],
-                     frame:str, header=None,
-                     obj:str=None,
-                     filt:str=None, exposure:str=None,
-                     method='sigmaclip', sigma=5,
-                     dir:str=''):
-        master = Reducer.combine(to_combine, method, sigma)
-        if frame == 'flat':
-            master = Reducer.normalize(master)
-        
-        # TODO: add history to header
+    @staticmethod 
+    def generate_filename(frame:str, obj:str=None,
+                          filt:str=None, exposure:str=None):
         # generate the right filename depending on what arguments are given
         file_name: str = f"master_{frame}"
         if obj is not None:
@@ -45,23 +36,30 @@ class Reducer():
         if exposure is not None:
             file_name = f"{file_name}_{exposure}s"
         file_name = f"{file_name}.fits"
-
-        Observation.safe_file(dir/file_name, master, header)
+        return file_name
     
     @staticmethod
     def normalize(data:np.ndarray):
-        inv_median = lambda x: 1 / np.median(x)
-        return data * inv_median(data)
+        return data / np.median(data)
 
-    def create_master_bias(self, keep_files:bool=False, force_new:bool=False):
+    def create_master_bias(self, force_new:bool=False, method:str=None):
         # check if master bias exists
         master_exist = False
         if not master_exist or force_new:
+            # collect data
             biases = [d for d in self.data.bias.data()]
             header = next(self.data.bias.headers())
+            # update header
             header['combined'] = True
             header['ncombine'] = len(biases)
-            master = self.stack_images(biases, 'bias', header, dir=self.data.reduced_path)
+            # stack the frames
+            method = self.method if method is None else method
+            master = self.combine(biases, method=method)
+            # save master
+            file_name = self.generate_filename('bias')
+            print(file_name)
+            Observation.safe_file(self.data.reduced_path/file_name, master, header)
+            # update the object
             self.data.masters['bias'] = master
-
+            
         return self.data.masters['bias']
