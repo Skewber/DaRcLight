dbf009ece8716c7f132bdd24a5d4d26c67a93e28
Skewber <jeremia.e.neuhold@gmail.com>
2025-06-04 20:02:33 +0200

    added function to enable logging

--- darclight/__init__.py
+++ darclight/__init__.py
@@ -1,24 +1,13 @@
 """darclight.__init__.py"""
-import logging
-import os
-from datetime import datetime
+from .utils import enable_logging
+from .io import DataCollection
+from .reduction import Reducer, inv_median
 
-LOG_DIR = './logs'
-os.makedirs(LOG_DIR, exist_ok=True)
+__version__ = "0.1.0"
 
-# generate filename
-timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-logfile = os.path.join(LOG_DIR, f"log_{timestamp}.log")
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger.propagate = False
-
-if not logger.hasHandlers():
-    # file handler
-    file_handler = logging.FileHandler(logfile)
-    file_handler.setLevel(logging.DEBUG)
-    file_handler.setFormatter(logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
-    ))
-    logger.addHandler(file_handler)
+__all__ = [
+    "DataCollection",
+    "Reducer",
+    "inv_median",
+    "enable_logging",
+]
---
+++ darclight/utils.py
@@ -0,0 +1,35 @@
+"""Module to provide utility tools for the reduction and analysis process."""
+import logging
+import os
+from datetime import datetime
+
+logger = logging.getLogger(__name__)
+
+def enable_logging(file:bool=True, console:bool=False, filename:str='log'):
+    """later"""
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.DEBUG)
+
+    if root_logger.hasHandlers():
+        root_logger.info("Logging is already enabled.")
+        return None
+
+    if file:
+        log_dir = './logs'
+        os.makedirs(log_dir, exist_ok=True)
+        timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+        logfile = os.path.join(log_dir, f"{filename}_{timestamp}.log")
+
+        file_handler = logging.FileHandler(logfile)
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S"))
+        root_logger.addHandler(file_handler)
+
+    if console:
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.DEBUG)
+        console_handler.setFormatter(logging.Formatter("%(levelname)s - %(message)s"))
+        root_logger.addHandler(console_handler)
+
+    root_logger.info("Logging has been enabled.")
