bd906028346ab049ee5e4d54876cad2d697414bc
Skewber <jeremia.e.neuhold@gmail.com>
2026-01-26 12:36:27 +0100

    migrated from lists and dicts to astrpy tables

--- darclight/io.py
+++ darclight/io.py
@@ -1,5 +1,8 @@
 """Module to manage general information like path to data"""
+import os
+from glob import iglob
 from fnmatch import fnmatch
+from collections.abc import Iterable
 import logging
 from functools import cached_property
 from collections import defaultdict
@@ -7,18 +10,27 @@ from pathlib import Path
 from typing import Generator, Tuple
 import numpy as np
 from astropy.io import fits
+from astropy.table import Table
+from astropy.time import Time
+import astropy.units as u
 
 logger = logging.getLogger(__name__)
+
+COMMON_KEYWORDS = {'BIAS':['bias', 'zero'],
+                   'DARK':['dark'],
+                   'FLAT':['flat'],
+                   'LIGHT':['light', 'science', 'object']}
+
 
 class DataCollection():
     """Class that organizes all files in a given directory.
     """
-    def __init__(self, raw_path:str|None=None, reduced_path:str|None=None, ignore:list|None=None):
+    def __init__(self, path:str|None=None, reduced_path:str|None=None, ignore:list|None=None):
         # ensure that both paths are an Path object
-        if raw_path is None:
-            self.raw_path = Path('.')
+        if path is None:
+            self.path = Path('.')
         else:
-            self.raw_path = Path(raw_path)
+            self.path = Path(path)
 
         if reduced_path is None:
             self.reduced_path = Path('./reduced')
@@ -28,113 +40,97 @@ class DataCollection():
         self.reduced_path.mkdir(exist_ok=True)
 
         self.ignore = [] if ignore is None else ignore
-        self.update_raw()
-        self.update_reduced()
+        self.scan(raw=True, reduced=True)
 
         logger.debug("DataCollection created")
 
     @staticmethod
-    def sort_files(path:str|Path, ignore:list|None=None)->Tuple[list,dict,dict,dict]:
-        """Reads all files from a directory and sorts them in seperate lists based on their header.
+    def _get_imagetype(keyword:str)->str:
+        keyword = keyword.lower()
 
-        :param path: Path to the data
-        :type path: str | Path
-        :return: A list of filenames for bias, dark, flat and light frames,
-                the dark frames are a dict with the exposure as key to the filelists,
-                the flat frames are a dict with the used filter as key to the filelists,
-                the light frames are a dict with the target as key to the filelists.
-        :rtype: Tuple[list,dict,dict,dict]
+        for typ, common_kwd in COMMON_KEYWORDS.items():
+            if any([kwd in keyword for kwd in common_kwd]):
+                return typ
+        raise RuntimeError('The keyword "%s" could not be matched to any known type. ' \
+        'Check the word or add a value to the "COMMON_KEYWORDS" variable.')
+
+    def _scan_raw(self):
+        # recreate the table
+        self.raw_files = Table(names=('ID', 'FILENAME',
+                                    'TYPE', 'DATE-OBS',
+                                    'NIGHT', 'JD',
+                                    'EXPOSURE', 'FILTER',
+                                    'TARGET'),
+                            dtype=('i4', 'U',
+                                    'U', 'U',
+                                    'U', 'f8',
+                                    'f4', 'U',
+                                    'U'))
+        # check files
+        for file in iglob(str(self.path)+'/**/*', recursive=True):
+            if (os.path.isdir(file) or 
+                any((fnmatch(file, pat) for pat in self.ignore)) or
+                fnmatch(file, f"*{self.reduced_path}*")):
+                # skip if directory or contains a pattern from the ignore list or is an reduced file
+                continue
+
+            hdr = fits.getheader(file)
+            self.raw_files.add_row((0,
+                                    file,
+                                    self._get_imagetype(hdr.get('IMAGETYP', 'None')),
+                                    hdr.get('DATE-OBS', '0000-00-00T00:00:00'),
+                                    '0000-00-00',
+                                    hdr.get('JD', 0),
+                                    hdr.get('EXPOSURE', -1),
+                                    hdr.get('FILTER', 'None'),
+                                    hdr.get('OBJECT', 'None')))
+
+    def _scan_reduced(self):
+        self.reduced_files = Table(names=('ID', 'FILENAME',
+                                        'TYPE', 'DATE',
+                                        'EXPOSURE', 'FILTER',
+                                        'OBJECT', 'COMBINED'),
+                                dtype=('i4', 'U',
+                                        'U', 'U',
+                                        'f4', 'U',
+                                        'U', 'bool'))
+        for file in iglob(str(self.reduced_path)+'/**/*', recursive=True):
+            if (os.path.isdir(file) or
+                any((fnmatch(file, pat) for pat in self.ignore))):
+                continue
+
+            hdr = fits.getheader(file)
+            self.reduced_files.add_row((0,
+                                        file,
+                                        self._get_imagetype(hdr['IMAGETYP']),
+                                        hdr.get('DATE', '0000-00-00T00:00:00'),
+                                        hdr.get('EXPOSURE', -1),
+                                        hdr.get('FILTER', 'None'),
+                                        hdr.get('OBJECT', 'None'),
+                                        hdr.get('COMBINED', False)))
+
+
+    def scan(self, raw:bool=True, reduced:bool=True):
+        """scans the directories from scractch
+
+        :param raw: whether or not the raw directory should be scaned, defaults to True
+        :type raw: bool, optional
+        :param reduced: whether or not the reduced directory should be scanned, defaults to True
+        :type reduced: bool, optional
         """
-        ignore = [] if ignore is None else ignore
-        if isinstance(path, str):
-            path = Path(path)
-        files = [f for f in path.iterdir()
-                 if f.is_file() and not any(fnmatch(f.name, pat) for pat in ignore)]
-        logger.info("Found %s file(s) in '%s'", len(files), str(path))
-
-        common_kwds = {'bias':['bias', 'zero'],
-                       'dark':['dark'],
-                       'flat':['flat'],
-                       'light':['light', 'science', 'object']}
-
-        # use defaultdicts to reduce the if/elif statements to check if an enty exists
-        bias_files = []
-        dark_files = defaultdict(list)
-        flat_files = defaultdict(list)
-        light_files = defaultdict(list)
-
-        for file in files:
-            header_kwd = fits.getval(file, 'imagetyp').lower()
-
-            if any([kwd in header_kwd for kwd in common_kwds['bias']]):
-                bias_files.append(file.name)
-
-            elif any(kwd in header_kwd for kwd in common_kwds['dark']):
-                exp = int(fits.getval(file, 'exposure'))
-                dark_files[exp].append(file.name)
-
-            elif any(kwd in header_kwd for kwd in common_kwds['flat']):
-                try:
-                    # this value migth not exist always
-                    used_filter = fits.getval(file, 'filter')
-                except KeyError:
-                    used_filter = None
-                flat_files[used_filter].append(file.name)
-
-            elif any(kwd in header_kwd for kwd in common_kwds['light']):
-                target = fits.getval(file, 'object')
-                light_files[target].append(file.name)
-
-        # convert to normal dicts since we don't need a default value outside this function
-        return bias_files, dict(dark_files), dict(flat_files), dict(light_files)
-
-    def update_raw(self):
-        """Rereads the raw file directory and recreates the list for each imagetyp.
-        """
-        bias, darks, flats, lights = self.sort_files(self.raw_path, self.ignore)
-        self.bias_files = bias
-        self.dark_files = darks
-        self.flat_files = flats
-        self.light_files = lights
-        logger.debug("Found the following raw file(s):\n"\
-                    "\t%s bias frame(s)\n"\
-                    "\t%s dark frame(s) for %s exposure(s)\n"\
-                    "\t%s flat frame(s) for %s filter(s)\n"\
-                    "\t%s light frame(s) for %s target(s)\n",
-                    len(bias),
-                    int(np.sum([len(frames) for frames in darks.values()])), len(darks),
-                    int(np.sum([len(frames) for frames in flats.values()])), len(flats),
-                    int(np.sum([len(frames) for frames in lights.values()])), len(lights))
-
-    def update_reduced(self):
-        """Rereads the reduced file directory and recreates the list for each imagetyp.
-        """
-        bias, darks, flats, lights = self.sort_files(self.reduced_path, self.ignore)
-        self.master_bias_file = bias[0] if len(bias)>0 else None
-        def validate(frames:dict, imgtype:str):
-            validated = {}
-            for key, file in frames.items():
-                if len(file) == 0:
-                    validated[key] = None
-                elif len(file) == 1:
-                    validated[key] = file[0]
-                else:
-                    raise ValueError(f"More than one master {imgtype} is detected for {key}.")
-            return validated
-        self.master_dark_files = {exp:None for exp in self.dark_exposures} | validate(darks, 'dark')
-        self.master_flat_files = {filt:None for filt in self.used_filters} | validate(flats, 'flat')
-        # skip validation for lights since they are usually stacked seperately
-        # => normally more than one
-        self.master_light_files = {tar:None for tar in self.targets} | lights
-        logger.debug("Found the following reduced file(s):\n"\
-                    "\t%s bias frame(s)\n"\
-                    "\t%s dark frame(s) for %s exposure(s)\n"\
-                    "\t%s flat frame(s) for %s filter(s)\n"\
-                    "\t%s light frame(s) for %s target(s)\n",
-                    len(bias),
-                    int(np.sum([len(frames) for frames in darks.values()])), len(darks),
-                    int(np.sum([len(frames) for frames in flats.values()])), len(flats),
-                    int(np.sum([len(frames) for frames in lights.values()])), len(lights))
+        if raw:
+            self._scan_raw()
+            # add value for the night --> avoid wrap over during midnight
+            self.raw_files['NIGHT'] = np.array((
+                Time(self.raw_files['DATE-OBS'])-12*u.hour
+                ).to_value('iso', subfmt='date'))
+            # sort by time and add unique ID to each file
+            self.raw_files.sort('JD')
+            self.raw_files['ID'] = np.arange(len(self.raw_files))
+            # group the files by necessary values
+            self.raw_files = self.raw_files.group_by(['TYPE', 'NIGHT', 'FILTER', 'EXPOSURE', 'TARGET'])
+        if reduced:
+            self._scan_reduced()
 
     @staticmethod
     def hdu_from_file(file:str)->Tuple[np.ndarray, fits.header.Header]:
@@ -173,7 +169,8 @@ class DataCollection():
         :return: list of filters
         :rtype: list[str]
         """
-        return list(self.flat_files.keys())
+        flats = self.raw_files[self.raw_files['TYPE']=='FLAT']
+        return list({str(f) for f in flats['FILTER']})
 
     @property
     def dark_exposures(self)->list[int]:
@@ -182,9 +179,10 @@ class DataCollection():
         :return: list of exposure times
         :rtype: list[int]
         """
-        return list(self.dark_files.keys())
+        darks = self.raw_files[self.raw_files['TYPE']=='DARK']
+        return list({int(e) for e in darks['EXPOSURE']})
 
-    @cached_property
+    @property
     def flat_exposures(self)->dict[str|None,set]:
         """exposure times for each filter
 
@@ -193,10 +191,9 @@ class DataCollection():
         """
         result = defaultdict(set)
 
-        for filt, files in self.flat_files.items():
-            for fname in files:
-                exp = fits.getval(self.raw_path/fname, "EXPOSURE")
-                result[filt].add(int(exp))
+        flats = self.raw_files[self.raw_files['TYPE']=='FLAT']
+        for filt, exp in flats['FILTER', 'EXPOSURE']:
+            result[str(filt)].add(int(exp))
         return dict(result)
 
     @property
@@ -206,7 +203,8 @@ class DataCollection():
         :return: list of targets
         :rtype: list[str]
         """
-        return list(self.light_files.keys())
+        lights = self.raw_files[self.raw_files['TYPE']=='LIGHT']
+        return [str(obj) for obj in set(lights['TARGET'])]
 
     @cached_property
     def light_meta(self)->dict[str,set[Tuple[str, int]]]:
@@ -219,19 +217,16 @@ class DataCollection():
         """
         result = defaultdict(set)
 
-        for target, files in self.light_files.items():
-            for fname in files:
-                header = fits.getheader(self.raw_path/fname)
-                filt = header.get("FILTER")
-                exp = header.get("EXPOSURE")
-                if exp is not None:
-                    result[target].add((filt, int(exp)))
+        lights = self.raw_files[self.raw_files['TYPE']=='LIGHT']
+        for target, filt, exp in lights['TARGET', 'FILTER', 'EXPOSURE']:
+            if exp is not None and filt is not None:
+                result[target].add((filt, int(exp)))
         logger.debug("Created meta data for lights:\n\t%s", result)
         return dict(result)
 
     @staticmethod
-    def generator(filelist:list, data:bool=True, header:bool=False,
-                  fname:bool=False, return_kwds:list[str]|None=None, **keywords)->Generator:
+    def file_data(filelist:Iterable[str], data:bool=True, header:bool=False,
+                  fname:bool=False, return_kwds:list[str]|None=None, **filter_kwds)->Generator:
         """generator to get the data and/or header of the files in the provided list.
 
         :param filelist: list of files to iterate over
@@ -245,12 +240,13 @@ class DataCollection():
         :param return_kwds: additional keywords that should be returned alongside the data/header,
                             defaults to None
         :type return_kwds: list[str] | None
-        :param keywords: additional keywords that should be extracted from the header
+        :param filter_kwds: additional keywords the returned data should be filtered for from the header
         :raises ValueError: if both (data and header) are set to False.
                             If you want only the filenames address the attribute directly.
         :yield: tuple of the desired outputs in the order (data, header, filename, return_kwd 1,...)
         :rtype: Tuple
         """
+        # TODO: include the possibility to only yield keywords?
         if not data and not header:
             raise ValueError("At least one of 'data' and 'header' must be True." \
                              f"You provided: data={data} and header={header}.")
@@ -259,9 +255,9 @@ class DataCollection():
         kwds = []
         for file in filelist:
             out = []
-            if keywords or return_kwd:
+            if filter_kwds or return_kwd:
                 hdr = fits.getheader(file)
-                if any(hdr.get(kwd) != val for kwd, val in keywords.items()):
+                if any(hdr.get(kwd) != val for kwd, val in filter_kwds.items()):
                     # skip if the keywords do not match
                     continue
                 for kwd in return_kwd:
@@ -272,11 +268,11 @@ class DataCollection():
             if header:
                 out.append(fits.getheader(file) if header else None)
             if fname:
-                out.append(file.name if fname else None)
+                out.append(file if fname else None)
             out = out + kwds    # add the desired keywords to the end
             yield tuple(out) if len(out)>1 else out[0]
 
-    def bias(self, data:bool=True, header:bool=False, fname:bool=False, **keywords)->Generator:
+    def bias(self, data:bool=True, header:bool=False, fname:bool=False, **filter_kwds)->Generator:
         """Generator to get the data and/or header of the files in the raw bias frames.
 
         :param data: whether or not the data of the file should be returned, defaults to True
@@ -290,10 +286,11 @@ class DataCollection():
         :yield: tuple of the desired outputs in the order (data, header, filename)
         :rtype: Tuple
         """
-        bias_files = [self.raw_path/f for f in self.bias_files]
-        return self.generator(bias_files, data, header, fname, **keywords)
+        bias = self.raw_files[self.raw_files['TYPE']=='BIAS']
+        bias_files = np.array(bias['FILENAME'])
+        return self.file_data(bias_files, data, header, fname, **filter_kwds)
 
-    def darks(self, exposure:int, data:bool=True, header:bool=False, fname:bool=False, **keywords)->Generator:
+    def darks(self, exposure:int, data:bool=True, header:bool=False, fname:bool=False, **filter_kwds)->Generator:
         """Generator to get the data and/or header of the files of the raw dark frames
           for a specific exposure.
 
@@ -314,11 +311,12 @@ class DataCollection():
         """
         if exposure not in self.dark_exposures:
             raise ValueError(f"There is no dark frame for this exposure: {exposure}")
-        dark_files = [self.raw_path/f for f in self.dark_files[exposure]]
-        return self.generator(dark_files, data, header, fname, **keywords)
+        darks = self.raw_files[self.raw_files['TYPE']=='DARK']
+        dark_files = np.array(darks['FILENAME'][darks['EXPOSURE']==exposure])
+        return self.file_data(dark_files, data, header, fname, **filter_kwds)
 
     def flats(self, used_filter:str|None, data:bool=True, header:bool=False, fname:bool=False,
-              return_kwds:list[str]|None=None, **keywords)->Generator:
+              return_kwds:list[str]|None=None, **filter_kwds)->Generator:
         """Generator to get the data and/or header of the files of the raw flat frames
           for a specific filter.
 
@@ -339,11 +337,12 @@ class DataCollection():
         """
         if used_filter not in self.used_filters:
             raise ValueError(f"There is no flat frame for this filter: {used_filter}")
-        flat_files = [self.raw_path/f for f in self.flat_files[used_filter]]
-        return self.generator(flat_files, data, header, fname, return_kwds, **keywords)
+        flats = self.raw_files[self.raw_files['TYPE']=='FLAT']
+        flat_files = np.array(flats['FILENAME'][flats['EXPOSURE']==used_filter])
+        return self.file_data(flat_files, data, header, fname, return_kwds, **filter_kwds)
 
     def lights(self, target:str, data:bool=True, header:bool=False, fname:bool=False,
-               return_kwds:list[str]|None=None, reduced:bool=False, **keywords)->Generator:
+               return_kwds:list[str]|None=None, reduced:bool=False, **filter_kwds)->Generator:
         """Generator to get the data and/or header of the files of the raw light frames
           for a specific target.
 
@@ -364,6 +363,6 @@ class DataCollection():
         """
         if target not in self.targets:
             raise ValueError(f"There is no light frame for the given target: {target}")
-        path = self.reduced_path if reduced else self.raw_path
-        light_files = [path/l for l in self.light_files[target]]
-        return self.generator(light_files, data, header, fname, return_kwds, **keywords)
+        lights = self.raw_files[self.raw_files['TYPE']=='LIGHT']
+        light_files = np.array(lights['FILENAME'][lights['TARGET']==target])
+        return self.file_data(light_files, data, header, fname, return_kwds, **filter_kwds)
